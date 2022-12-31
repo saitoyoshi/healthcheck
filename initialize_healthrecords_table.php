e5e7964f4302341bedd7929587fe876d2f8cb7be
saitoyoshi <saitoyoshi@gmail.com>
2022-12-31 09:48:12 +0900

    DB作成スクリプトを追加

---
+++ initialize_healthrecords_table.php
@@ -0,0 +1,22 @@
+<?php
+
+$mysqli = new mysqli('db', 'test_user', 'pass', 'test_database');
+if ($mysqli->connect_error) {
+    throw new RuntimeException('mysqli接続エラー： ' . $mysqli->connect_error);
+}
+
+$mysqli->query('DROP TABLE IF EXISTS healthrecords');
+$createTableSql = <<< EOT
+CREATE TABLE healthrecords (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    physical_condition INT NOT NULL,
+    mood_state INT NOT NULL,
+    back_pain INT NOT NULL,
+    eyestrain INT NOT NULL,
+    headache INT NOT NULL,
+    recording DATE NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4
+EOT;
+$mysqli->query($createTableSql);
+$mysqli->close();
